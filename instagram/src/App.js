ffcee84c9d4ed4334726196288654b8a50029068
Brandon-Pampuch <brandonpampuch@gmail.com>
2019-04-30 09:06:25 -0500

    add functional like button

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -13,11 +13,19 @@ class App extends React.Component {
     };
   }
 
-  addHeart = key => {
-    this.setState({
-      ...this.state,
-
-    });
+  addHeart = id => {
+    this.setState(prevState =>({
+      data: prevState.data.map(post => {
+        if (post.id === id) {
+          return {
+            ...post,
+            likes: post.likes + 1
+          };
+        } else {
+          return post;
+        }
+      })
+    }));
   };
 
   render() {
